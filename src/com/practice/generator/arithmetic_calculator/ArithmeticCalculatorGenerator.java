5ccbc582537abdd9631635946b17e6f183d78210
dongsub_youn <dongsub_youn@tmax.co.kr>
2022-03-14 14:55:50 +0900

    feat(generator): 사칙연산을 가능하게 하는 클래스에 대한 generator 추가
    외부에서 경로와 클래스명을 받으며, 생성하고자 하는 메소드를 추가할 수 있는 generator 추가

---
+++ src/com/practice/generator/arithmetic_calculator/ArithmeticCalculatorGenerator.java
@@ -0,0 +1,50 @@
+package com.practice.generator.arithmetic_calculator;
+
+import com.squareup.javapoet.JavaFile;
+import com.squareup.javapoet.MethodSpec;
+import com.squareup.javapoet.TypeSpec;
+
+import javax.lang.model.element.Modifier;
+import java.io.IOException;
+import java.nio.file.Paths;
+
+public class ArithmeticCalculatorGenerator {
+    TypeSpec.Builder typeSpecBuilder;
+    public ArithmeticCalculatorGenerator(String className) {
+        this.typeSpecBuilder = TypeSpec.classBuilder(className)
+                .addModifiers(Modifier.PUBLIC, Modifier.FINAL);
+    }
+
+    public void generateJavaSourceFile(String packageName) {
+        TypeSpec typeSpec = this.typeSpecBuilder.build();
+        JavaFile javaFile = JavaFile.builder(packageName, typeSpec).build();
+
+        try {
+            javaFile.writeTo(Paths.get("./src"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void addMethodToClass(String methodName, int defaultValue, int from, int to, String operation) {
+        this.typeSpecBuilder.addMethod(this.generateMethod(methodName, defaultValue, from, to, operation));
+    }
+
+    public MethodSpec generateMethod(String methodName, int defaultValue, int from, int to, String operation) {
+        return MethodSpec.methodBuilder(methodName)
+                .returns(int.class)
+                .addStatement("int result = " + defaultValue)
+                .beginControlFlow("for (int i = " + from + "; i < " + to + "; i++)")
+                .addStatement("result = result " + operation + " i")
+                .endControlFlow()
+                .addStatement("return result")
+                .build();
+    }
+
+    public static void main(String[] args) {
+        ArithmeticCalculatorGenerator arithmeticCalculatorGenerator = new ArithmeticCalculatorGenerator("ArithmeticCalculator");
+        arithmeticCalculatorGenerator.addMethodToClass("add1to50", 0, 1, 50, "+");
+        arithmeticCalculatorGenerator.addMethodToClass("multiply5to20", 1, 5, 20, "*");
+        arithmeticCalculatorGenerator.generateJavaSourceFile("com.example.arithmetic_calculator");
+    }
+}
